5ef5fad527248ba1b1b643b3a351939bfc91c084
Filip Liška <filipliska42@gmail.com>
2026-04-23 15:48:41 +0200

    3.commit

--- sorting.py
+++ sorting.py
@@ -1,23 +1,51 @@
 import random
+import matplotlib.pyplot as plt
 
 def random_numbers(count, low=0, high=100):
     return [random.randint(low, high) for _ in range(count)]
 
 def selection_sort(values):
-    x = len(values)
+    val = values.copy()
+    x = len(val)
     for i in range(x):
         min = i
         for j in range(i+1, x):
-            if values[j] < values[min]:
+            if val[j] < val[min]:
                 min = j
-        values[i], values[min] = values[min], values[i]
-    return values
+        val[i], val[min] = val[min], val[i]
+    return val
 
+def bubble_sort(values):
+    val2 = values.copy()
+    x = len(val2)
+    plt.ion()
+    plt.show()
+    for i in range(x):
+        for j in range(0, x-1-i):
+            if val2[j] > val2[j+1]:
+                val2[j], val2[j+1] = val2[j+1], val2[j]
+            index_highlight1 = j
+            index_highlight2 = j + 1
+            colors = ["steelblue"] * len(values)
+            colors[index_highlight1] = "tomato"
+            colors[index_highlight2] = "tomato"
+            plt.clf()
+            plt.bar(range(len(values)), values, color=colors)
+            plt.title("Bubble Sort")
+            plt.pause(0.1)
+    plt.ioff()
+    plt.show()
+    return val2
 
 
 if __name__ == "__main__":
     values = random_numbers(10)  # 10 čísel v rozsahu 0–100
+    print("Data:")
     print(values)  # např. [42, 7, 91, 15, 63, 8, 57, 73, 2, 100]
     selection = selection_sort(values)
+    print("Selection:")
     print(selection)
+    bubble = bubble_sort(values)
+    print("Bubble:")
+    print(bubble)
     small = random_numbers(5, low=0, high=20)  # 5 čísel v rozsahu 0–20
